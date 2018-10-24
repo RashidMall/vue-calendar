f28e91725e0fddd9d4a52174cd1fa835120abf96
RashidMall <mr.mallaev@gmail.com>
2018-10-24 15:34:05 +0200

    Put things in place

---
+++ src/app/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <div id="calendar-week" class="container">
+            <!-- Calendar week -->
+        </div>
+        <div id="calendar-entry">
+            <!-- Calendar entry -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+<style lang="scss">
+html, body {
+    height: 100%;
+}
+#app {
+    /* SCSS for app */
+}
+#calendar-week {
+    /* SCSS for calendar week */
+}
+#calendar-entry {
+    /* SCSS for calendar entry */
+}
+</style>
+
+
