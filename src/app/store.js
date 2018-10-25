a2060f056080d437418659f6b7fb569d42f12b1d
RashidMall <mr.mallaev@gmail.com>
2018-10-25 15:36:55 +0200

    Add setActiveDay method

--- src/app/store.js
+++ src/app/store.js
@@ -4,7 +4,19 @@ export const store = {
     state: {
         seedData
     },
-    getActiveDay () {
+    /**
+     *  return the day object that has active: true
+     */
+    getActiveDay(){
         return this.state.seedData.find(day => day.active);
+    },
+    /**
+     * sets the selected day to active: true 
+     * and all other day objects to active: false
+     */
+    setActiveDay(dayId){
+        this.state.seedData.map((dayObj) => {
+            dayObj.id === dayId ? dayObj.active = true : dayObj.active = false;
+        });
     }
 }
