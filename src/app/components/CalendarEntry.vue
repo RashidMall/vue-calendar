779924f05f04cad537f170e615472367bccacc4f
RashidMall <mr.mallaev@gmail.com>
2018-10-25 16:13:43 +0200

    Add calendar entry

--- src/app/components/CalendarEntry.vue
+++ src/app/components/CalendarEntry.vue
@@ -1,12 +1,25 @@
 <template>
     <div id="calendar-entry">
-        <!-- Calendar entry -->
+        <div class="calendar-entry-note">
+            <input type="text" name="" id="" placeholder="New Event">
+            <p class="calendar-entry-day">
+                Day of event: <span class="bold">{{ titleOfActiveDay }}</span>
+            </p>
+            <a href="" class="btn btn-primary btn-sm">Submit</a>
+        </div>
     </div>
 </template>
 
 <script>
+import { store } from '../store.js';
+
 export default {
-    name: 'CalendarEntry'
+    name: 'CalendarEntry',
+    computed: {
+        titleOfActiveDay(){
+            return store.getActiveDay().fullTitle;
+        }
+    },
 }
 </script>
 
