8e05ccd04eecb141b50f8f7cae0e2c67b1e8e56e
RashidMall <mr.mallaev@gmail.com>
2018-10-25 11:40:52 +0200

    Add a column structure in the template

---
+++ src/app/components/CalendarDay.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="day col">
+        <div class="day-banner text-center">{{ day.abbvTitle }}</div>
+        <div class="day-details">
+            <div class="day-number">{{ day.id }}</div>
+            <div class="day-event">
+                <div>
+                    <span class="text-center details">Get Groceries</span>
+                    <div class="text-center icons">
+                        <i class="fa fa-pencil-square edit-icon"></i>
+                        <i class="fa fa-trash-o delete-icon"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'CalendarDay',
+    props: ['day']
+}
+</script>
