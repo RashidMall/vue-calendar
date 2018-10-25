5c0822b81609243585aa028fe48141f0550eff66
RashidMall <mr.mallaev@gmail.com>
2018-10-25 14:29:42 +0200

    Add random background color to events

--- src/app/components/CalendarEvent.vue
+++ src/app/components/CalendarEvent.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="day-event">
+    <div class="day-event" :style="getEventBackgroundColor">
         <div>
-            <span class="text-center details">Get Groceries</span>
+            <span class="text-center details">{{ event.details }}</span>
             <div class="text-center icons">
                 <i class="fas fa-pen-square edit-icon"></i>
                 <i class="fas fa-trash-alt delete-icon"></i>
@@ -9,6 +9,21 @@
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'CalendarEvent',
+    props: ['event', 'day'],
+    computed: {
+        getEventBackgroundColor(){
+            const colors = ['#3D9970', '#7FDBFF', '#01FF70', '#FF851B', '#AAAAAA', '#DDDDDD', '#F012BE', '#FF4136', '#FFDC00', '#2ECC40', '#39CCCC'];
+            let randomColor = colors[Math.floor(Math.random() * colors.length)];
+            return `background-color: ${randomColor}`;
+        }
+    }
+}
+</script>
+
 
 <style lang="scss" scoped>
 .day-event {
@@ -21,7 +36,7 @@
     .details {
         display: block;
     }
-    .icons .fas {
+    .fas {
         padding: 2px;
     }
     input {
