d57ed60d461f69392e7c4d54610b0d99fb00e914
RashidMall <mr.mallaev@gmail.com>
2018-10-25 16:33:01 +0200

    Create the click handler and set up the setActiveDay() method

--- src/app/components/CalendarDay.vue
+++ src/app/components/CalendarDay.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="day col">
+    <div class="day col" @click="setActiveDay(day.id)">
         <div class="day-banner text-center">{{ day.abbvTitle }}</div>
         <div class="day-details">
             <div class="day-number">{{ day.id }}</div>
@@ -13,11 +13,17 @@
 </template>
 
 <script>
+import { store } from '../store.js';
 import CalendarEvent from './CalendarEvent.vue';
 
 export default {
     name: 'CalendarDay',
     props: ['day'],
+    methods: {
+        setActiveDay(dayId){
+            store.setActiveDay(dayId);
+        }
+    },
     components: {
         CalendarEvent
     }
